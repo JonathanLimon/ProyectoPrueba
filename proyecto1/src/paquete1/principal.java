27dc8a0e65f7be2050c4fd8c2444d07c5ec935b2
in1dw3 <in1dw3@HZ352015.elorrieta.int>
2023-01-27 15:46:14 +0100

    Primera modificacion

--- proyecto1/src/paquete1/principal.java
+++ proyecto1/src/paquete1/principal.java
@@ -5,6 +5,7 @@ public class principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Estamos empezando el proyecto con Git");
+		System.out.println("Primera modificacion del proyecto");
 	}
 
 }
