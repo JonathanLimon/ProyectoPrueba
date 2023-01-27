3f40b0758d62a6cddc99cb727e784cd62c7a9e3c
in1dw3 <in1dw3@HZ352015.elorrieta.int>
2023-01-27 17:21:16 +0100

    Tercera modificacion

--- proyecto1/src/paquete1/principal.java
+++ proyecto1/src/paquete1/principal.java
@@ -7,6 +7,7 @@ public class principal {
 		System.out.println("Estamos empezando el proyecto con Git");
 		System.out.println("Primera modificacion del proyecto");
 		System.out.println("Segunda modificacion del proyecto");
+		System.out.println("Tercera modificacion del proyecto");
 
 	}
 
